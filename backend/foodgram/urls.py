5ab7d34680f18b6efd76ed63ce01fe06713c7d2d
Даниил Тоненьков <dtonenkov@mail.ru>
2024-10-08 00:02:00 +0300

    dltmh

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("r/", include("redirect.urls")),
+    path("api/r/", include("redirect.urls")),
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
 ]
