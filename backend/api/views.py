5bc3e5df02d359039e7f214878d2e77eaf605003
Даниил Тоненьков <dtonenkov@mail.ru>
2024-10-07 18:48:16 +0300

    fsgmfsgjkdfsfdsknjhhjsfg

--- backend/api/views.py
+++ backend/api/views.py
@@ -33,7 +33,7 @@ from .serializers import (AvatarSerializer, CustomAuthTokenSerializer,
 def redirect_to_full_link(request, short_code):
     """View для перенаправления короткой ссылки на полный URL рецепта."""
     try:
-        base_url = request.build_absolute_uri("/")[:-2].strip("/")
+        base_url = request.build_absolute_uri("/")[:-3].strip("/")
         recipe_id = short_url.decode_url(short_code)
         recipe = get_object_or_404(Recipe, id=recipe_id)
         full_url = f"{base_url}/recipes/{recipe.id}/"
