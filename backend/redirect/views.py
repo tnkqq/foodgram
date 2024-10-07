a7426c912c5a202d998721dec06b57fe10d52e1e
Даниил Тоненьков <dtonenkov@mail.ru>
2024-10-07 23:49:11 +0300

    grfkmgkfd

--- backend/redirect/views.py
+++ backend/redirect/views.py
@@ -1,7 +1,6 @@
 import short_url
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect
-from django.urls import reverse
 
 from .models import Recipe
 
@@ -11,7 +10,11 @@ def redirect_to_full_link(request, short_code):
     try:
         recipe_id = short_url.decode_url(short_code)
         recipe = get_object_or_404(Recipe, id=recipe_id)
-        full_url = f"{request.scheme}://{request.get_host()}/recipes/{recipe.id}/"
+        full_url = (
+            f"{request.scheme}://"
+            f"{request.get_host()}/"
+            f"recipes/{recipe.id}/"
+        )
         return redirect(full_url)
     except (ValueError, Recipe.DoesNotExist):
         return HttpResponse(
