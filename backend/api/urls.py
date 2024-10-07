0df76711ee98bf90b7f0c1eec0ddf80c22547d5f
Даниил Тоненьков <dtonenkov@mail.ru>
2024-10-07 17:32:19 +0300

    mfmgvs

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,7 +2,8 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import (CustomAuthToken, IngredientViewSet, LogoutView,
-                    RecipeViewSet, TagViewSet, UserViewSet)
+                    RecipeViewSet, TagViewSet, UserViewSet,
+                    redirect_to_full_link)
 
 router_v1 = DefaultRouter()
 
@@ -18,6 +19,10 @@ router_v1.register("recipes", viewset=RecipeViewSet, basename="recipes")
 router_v1.register(r"users", UserViewSet, basename="users")
 
 url_auth = [
+    path(
+        "r/<str:short_code>/",
+        redirect_to_full_link,
+        name="short-link-redirect"),
     path("token/login/", CustomAuthToken.as_view(), name="token_obtain"),
     path("token/logout/", LogoutView.as_view(), name="logout"),
 ]
