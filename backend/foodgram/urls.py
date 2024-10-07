e2856b85464b93c938604d4a1662210a00a655d1
Даниил Тоненьков <dtonenkov@mail.ru>
2024-10-07 17:46:15 +0300

    fmnkgb

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -1,9 +1,14 @@
+from api.views import redirect_to_full_link
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path(
+        "r/<str:short_code>/",
+        redirect_to_full_link,
+        name="short-link-redirect"),
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
 ]
