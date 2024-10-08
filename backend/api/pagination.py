e1d9f4c0051c273ccdc191b2977d1e8d2886d9b0
Даниил Тоненьков <dtonenkov@mail.ru>
2024-10-09 01:15:28 +0300

    m,efmbf

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -11,10 +11,10 @@ class UserSubscriptionPagination(PageNumberPagination):
     """custom pagination for user and subs."""
 
     page_size = 10
-    page_size_query_param = "limit"
+    limit_size_query_param = "limit"
     max_page_size = 100
 
 
 class PageLimitPagination(PageNumberPagination):
     page_size = 6
-    page_size_query_param = 'limit'
+    limit_size_query_param = 'limit'
