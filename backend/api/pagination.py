ceb77e6fc770e4a576eb131184a729c17e07b625
Даниил Тоненьков <dtonenkov@mail.ru>
2024-10-09 01:04:03 +0300

    fkmdbmd

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -18,4 +18,3 @@ class UserSubscriptionPagination(PageNumberPagination):
 class PageLimitPagination(PageNumberPagination):
     page_size = 6
     page_size_query_param = 'limit'
-    max_page_size = 10000
--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -47,7 +47,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
     ],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "DEFAULT_PAGINATION_CLASS": "api.pagination.PageLimitPagination",
     "PAGE_SIZE": 6,
 }
 
