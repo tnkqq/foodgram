b21cb1783e0eefa35f918df9be8ccd4e8137ea70
Даниил Тоненьков <dtonenkov@mail.ru>
2024-10-09 02:20:24 +0300

    fmsg

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -18,5 +18,5 @@ class UserSubscriptionPagination(PageNumberPagination):
 class PageLimitPagination(PageNumberPagination):
     page_size = 6
     page_size_query_param = 'limit'
-    max_page_size = 100x
+    max_page_size = 100
     page_query_param = 'page'
