e9fe1ddd6e4b60602015e9f3234133035ef0544b
Даниил Тоненьков <dtonenkov@mail.ru>
2024-10-09 01:43:57 +0300

    flsblfmsb

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -16,20 +16,7 @@ class UserSubscriptionPagination(PageNumberPagination):
 
 
 class PageLimitPagination(PageNumberPagination):
-    page_size = None
+    page_size = 6
     page_size_query_param = 'limit'
     max_page_size = 100
-
-    def paginate_queryset(self, queryset, request, view=None):
-        page_size = self.get_page_size(request)
-        if not page_size:
-            return None
-
-        paginator = self.django_paginator_class(queryset, page_size)
-        page_number = self.get_page_number(request, paginator)
-        self.page = paginator.page(page_number)
-
-        if self.page.number > paginator.num_pages:
-            self.page = paginator.page(paginator.num_pages)
-
-        return list(self.page)
+    page_query_param = 'page'
